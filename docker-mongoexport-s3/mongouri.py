814aa9a27a417f798412d22775714a3fcc32ef22
Greg Sochanik <greg.sochanik@7digital.com>
2017-06-12 15:53:29 +0100

    Add ability to specify auth database

--- docker-mongoexport-s3/mongouri.py
+++ docker-mongoexport-s3/mongouri.py
@@ -7,6 +7,7 @@ auth = '' if data['username'] is None else '-u %s -p %s' % (data['username'], da
 host = '-h %s:%s' % (data['nodelist'][0][0], data['nodelist'][0][1])
 dbname = '-d %s' % data['database']
 collection = '' if os.environ.get('MONGO_COLLECTION') is None else '-c %s' % os.environ.get('MONGO_COLLECTION')
-options = '%s %s %s %s' % (auth, host, dbname, collection)
+authDatabase = '' if os.environ.get('MONGO_AUTH_DATABASE') is None else '--authenticationDatabase %s' % os.environ.get('MONGO_AUTH_DATABASE')
+options = '%s %s %s %s %s' % (auth, host, dbname, collection, authDatabase)
 
 print options
